123691f8ed5c77261a11735648f1bb1ce5e330fd
Peter Keating <pete.keating@etchuk.com>
2021-09-30 12:37:32 +0100

    v0.1.1

--- Manifest.cs
+++ Manifest.cs
@@ -5,6 +5,6 @@ using OrchardCore.Modules.Manifest;
     Category = "Content",
     Description = "Control which fonts are loaded based on conditions.",
     Name = "Fonts",
-    Version = "0.1.0",
+    Version = "0.1.1",
     Website = "https://etchuk.com"
 )]
